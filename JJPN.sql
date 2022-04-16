5c07d2d0f79b6b73dc1669977b78924b7e760145
NGYIKLOK <99123007+NGYIKLOK@users.noreply.github.com>
2022-04-16 15:50:02 +0800

    Revert "Revert "new sql file""
    This reverts commit 8aceba054521b6cc2f5ff09b7ceaa50c53364e02.

---
+++ JJPN.sql
@@ -0,0 +1,10 @@
+create database if not exists `JJPN`;
+use  `JJPN`;
+create table location(
+`locationid` int(11) not null,
+'name' varchar(20) not null,
+'contact' varchar(8) not null,
+'address' varchar(255),
+'district' varchar(50),
+primary key (`locationid`),
+) charset=utf8;
