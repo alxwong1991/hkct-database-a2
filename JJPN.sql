8974bafd0cdf76cf5ea6e7f3627919ed94194164
炎舞雪 <96097420+EnmaYuki@users.noreply.github.com>
2022-04-19 20:01:04 +0800

    update of tables

--- JJPN.sql
+++ JJPN.sql
@@ -1,10 +1,44 @@
-create database if not exists `JJPN`;
-use  `JJPN`;
-create table location(
-`locationid` int(11) not null,
-`name` varchar(20) not null,
-`contact` varchar(8) not null,
-`address` varchar(255),
-`district` varchar(50),
-primary key (`locationid`),
-) charset=utf8;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3305
+-- Generation Time: Apr 19, 2022 at 11:59 AM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jjpn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `C_ID` int NOT NULL,
+  `C_productName` int NOT NULL,
+  `C_price` int NOT NULL,
+  `C_priceTotal` int NOT NULL,
+  `C_quantity` int NOT NULL,
+  `PL_ID` int NOT NULL,
+  PRIMARY KEY (`C_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
